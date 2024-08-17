e32f1974dccd81af28222144e554d909e04a4e17
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-17 23:50:11 +0300

    Add paragraphs and updated app.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,8 +9,18 @@ const About = () => {
                     <AnimatedLetters strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
                 </h1>
 
+                <p> pariatur irure irure irure labore et nulla deserunt ex occaecat labore aliqua commodo consequat minim esse tempor voluptate id ea occaecat occaecat dolore nostrud elit magna tempor non est cillum
+                </p>
+                <p>
+                    quis sit aliqua in velit ea deserunt voluptate commodo in irure quis voluptate velit magna eu do dolore enim dolore veniam aute nulla enim dolor excepteur do ipsum magna ipsum quis aute amet quis eiusmod
+                </p>
+                <p>
+                    culpa voluptate exercitation consequat esse aliquip nulla fugiat labore veniam tempor do incididunt incididunt deserunt consectetur consequat ipsum ut nulla id ea ut commodo elit voluptate pariatur et qui ad consectetur consectetur anim
+                </p>
+
             </div>
         </div>
+
     )
 }
 
