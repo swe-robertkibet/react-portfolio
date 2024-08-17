e17c2dcecc61d3a09740b3b7632f14200b6dd55d
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-18 02:17:11 +0300

    Add animated heading to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,29 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react';
 
 const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+
+        return () => clearTimeout(timer);
+    }, [])
 
     return (
         <>
             <div className='container contact-page'>
 
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} idx={15} />
+                    </h1>
+                </div>
             </div>
 
             <Loader type='pacman' />
