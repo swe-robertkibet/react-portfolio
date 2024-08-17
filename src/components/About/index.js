048822256516f0077aad59d6a732ce6f82adf67a
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-18 00:23:12 +0300

    Add layout styles to paragraph about page, added rubberband effect on h1 on hover

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,24 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react';
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+
+        return () => clearTimeout(timer);
+    }, [])
+
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
-                    <AnimatedLetters strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
+                    <AnimatedLetters letterClass={letterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
                 </h1>
 
                 <p> pariatur irure irure irure labore et nulla deserunt ex occaecat labore aliqua commodo consequat minim esse tempor voluptate id ea occaecat occaecat dolore nostrud elit magna tempor non est cillum
