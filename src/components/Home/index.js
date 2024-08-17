d193476a33bdf455ff2fcba05a167d7e70b776a0
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-18 02:04:55 +0300

    Updated homepage to include loader animation styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [displayedName, setDisplayedName] = useState('');
@@ -113,35 +114,38 @@ const Home = () => {
     };
 
     return (
-        <div className="container home-page">
-            <div className="text-zone" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
+        <>
+            <div className="container home-page">
+                <div className="text-zone" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
 
-                    <span className="scramble-letter">&nbsp;</span>
-                    {displayedName.split('').map((letter, index) => (
-                        <React.Fragment key={index}>
-                            {index === 6 && <span className="scramble-letter">&nbsp;</span>}
-                            <span
-                                className={`scramble-letter ${index === 0 || index === 6 ? 'highlight-letter' : ''}`}
-                                style={{ animationDelay: `${index * 0.1}s` }}
-                            >
-                                {letter}
-                            </span>
-                        </React.Fragment>
-                    ))}
+                        <span className="scramble-letter">&nbsp;</span>
+                        {displayedName.split('').map((letter, index) => (
+                            <React.Fragment key={index}>
+                                {index === 6 && <span className="scramble-letter">&nbsp;</span>}
+                                <span
+                                    className={`scramble-letter ${index === 0 || index === 6 ? 'highlight-letter' : ''}`}
+                                    style={{ animationDelay: `${index * 0.1}s` }}
+                                >
+                                    {letter}
+                                </span>
+                            </React.Fragment>
+                        ))}
 
-                    <br />
-                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
-                </h1>
-                <h2>Problem solver | Innovator | Learner</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+                    </h1>
+                    <h2>Problem solver | Innovator | Learner</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
