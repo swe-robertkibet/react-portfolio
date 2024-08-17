1a6704f552735a16719b95355cad79340505379b
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-18 02:11:24 +0300

    Add contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import Loader from 'react-loaders'
+import './index.scss'
+
+const Contact = () => {
+
+    return (
+        <>
+            <div className='container contact-page'>
+
+            </div>
+
+            <Loader type='pacman' />
+        </>
+    )
+}
+
+export default Contact
