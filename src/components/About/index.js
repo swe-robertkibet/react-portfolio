14fa57d93f867250033011af0655840edbb4ce70
swe-robertkibet <swe.robertkibet@gmail.com>
2024-08-31 20:34:57 +0300

    refactor(About): Remove unused imports and clean up code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,9 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faNodeJs,
-    faReact,
-    faJava,
-    faCss3,
-    faJs,
-    faGitAlt
-} from '@fortawesome/free-brands-svg-icons';
 import Loader from 'react-loaders';
 
 const About = () => {
-
     const [letterClass, setLetterClass] = useState('text-animate');
 
     useEffect(() => {
@@ -35,9 +25,7 @@ const About = () => {
                     <p>I am a passionate Software Engineer with over 2 years of experience. My journey in technology began with a deep curiosity about how things work, which naturally evolved into a love for programming and software development.</p>
                     <p>Throughout my career, I have had the opportunity to work on various projects that have challenged and honed my skills. From developing a comprehensive web application that integrates user authentication and payment processing to creating innovative solutions like a Process Scheduling Simulator and a Gaming PC Builder tool, I have demonstrated my ability to tackle complex problems and deliver impactful results.</p>
                     <p>Beyond my technical skills, I am a strong advocate for continuous learning and knowledge sharing. I am always eager to take on new challenges and look forward to contributing my skills and passion to exciting software engineering roles.</p>
-
                 </div>
-
             </div>
             <Loader type="pacman" />
         </>
